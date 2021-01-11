f32e0a0a438c6f5aa0f3cdab6574b367303556ec
Hoshi <hoshimure.47@gmail.com>
2021-01-11 16:22:09 +0900

    :+1: github api call

--- src/main.rs
+++ src/main.rs
@@ -2,46 +2,64 @@ use actix_web::{
     client::{Client, Connector},
     get, post, web, App, HttpResponse, HttpServer, Responder,
 };
+use dotenv::dotenv;
 use openssl::ssl::{SslConnector, SslMethod};
+use serde::{Deserialize, Serialize};
+use serde_json::Value;
+use std::env;
 
-#[get("/")]
-async fn hello() -> impl Responder {
-    HttpResponse::Ok().body("Hello World.")
+#[derive(Debug, Serialize, Deserialize)]
+struct Actor {
+    pub id: u64,
+    pub login: String,
+    pub url: String,
+    pub avatar_url: String,
 }
 
-#[post("/echo")]
-async fn echo(req_body: String) -> impl Responder {
-    HttpResponse::Ok().body(req_body)
+#[derive(Debug, Serialize, Deserialize)]
+struct Repo {
+    pub id: u64,
+    pub name: String,
+    pub url: String,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+struct GitHubEvent {
+    pub id: String,
+    pub actor: Actor,
+    pub repo: Repo,
 }
 
 #[get("/v1/activities/github")]
 async fn get_activities_from_github() -> impl Responder {
+    let url = "https://api.github.com/users/yahooshiken/events";
+    let token = env::var("GITHUB_TOKEN").expect("VAR is not defined");
+
     let builder = SslConnector::builder(SslMethod::tls()).unwrap();
     let client = Client::builder()
         .header("User-Agent", "localhost")
+        .header("Authorization", format!("token: {}", token))
         .connector(Connector::new().ssl(builder.build()).finish())
         .finish();
-    let url = "https://api.github.com/zen";
-    let payload = client.get(url).send().await;
-    println!("Response: {:?}", payload);
 
-    return HttpResponse::Ok().body("Hello World.");
-}
+    let response = client.get(url).send().await;
+    let body = response.unwrap().body().await.unwrap();
+    let body_str = String::from_utf8(body.to_vec()).unwrap();
+    let events: Vec<GitHubEvent> = serde_json::from_str(&body_str).unwrap();
 
-async fn manual_hello() -> impl Responder {
-    HttpResponse::Ok().body("Hey, there!")
+    for event in events.iter() {
+        println!("Body: {:?}", event);
+    }
+
+    return HttpResponse::Ok().json(events);
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| {
-        App::new()
-            .service(hello)
-            .service(echo)
-            .service(get_activities_from_github)
-            .route("/hey", web::get().to(manual_hello))
-    })
-    .bind("127.0.0.1:8080")?
-    .run()
-    .await
+    dotenv().ok();
+
+    return HttpServer::new(|| App::new().service(get_activities_from_github))
+        .bind("127.0.0.1:8080")?
+        .run()
+        .await;
 }
