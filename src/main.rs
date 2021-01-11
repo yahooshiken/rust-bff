b1d7bd6a5c40161b23c854e3c41906ec0108d5ff
Hoshi <hoshimure.47@gmail.com>
2021-01-11 16:36:34 +0900

    :+1: modified ip.

--- src/main.rs
+++ src/main.rs
@@ -58,7 +58,7 @@ async fn main() -> std::io::Result<()> {
     dotenv().ok();
 
     return HttpServer::new(|| App::new().service(get_activities_from_github))
-        .bind("127.0.0.1:8080")?
+        .bind("0.0.0.0:8080")?
         .run()
         .await;
 }
