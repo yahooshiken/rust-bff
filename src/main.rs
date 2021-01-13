1d20a54d5ab58d1b27e18eaf5381f01afdff4299
Hoshi <hoshimure.47@gmail.com>
2021-01-13 15:19:22 +0900

    :+1: add twitter api.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use actix_cors::Cors;
 use actix_web::{
     client::{Client, Connector},
     get, App, HttpResponse, HttpServer, Responder,
@@ -5,6 +6,7 @@ use actix_web::{
 use dotenv::dotenv;
 use openssl::ssl::{SslConnector, SslMethod};
 use serde::{Deserialize, Serialize};
+use serde_json::Value;
 use std::env;
 
 #[derive(Debug, Serialize, Deserialize)]
@@ -29,6 +31,26 @@ struct GitHubEvent {
     pub repo: Repo,
 }
 
+#[get("/v1/activities/twitter")]
+async fn get_activities_from_twitter() -> impl Responder {
+    let url = "https://api.twitter.com/2/users/1084336920/tweets?tweet.fields=created_at&expansions=author_id&user.fields=created_at&max_results=10";
+    let token = env::var("TWITTER_BEARER_TOKEN").expect("VAR is not defined");
+
+    let builder = SslConnector::builder(SslMethod::tls()).unwrap();
+    let client = Client::builder()
+        .header("User-Agent", "localhost")
+        .header("Authorization", format!("Bearer {}", token))
+        .connector(Connector::new().ssl(builder.build()).finish())
+        .finish();
+
+    let response = client.get(url).send().await;
+    let body = response.unwrap().body().await.unwrap();
+    let body_str = String::from_utf8(body.to_vec()).unwrap();
+    let events: Value = serde_json::from_str(&body_str).unwrap();
+
+    return HttpResponse::Ok().body(events);
+}
+
 #[get("/v1/activities/github")]
 async fn get_activities_from_github() -> impl Responder {
     let url = "https://api.github.com/users/yahooshiken/events";
@@ -46,10 +68,6 @@ async fn get_activities_from_github() -> impl Responder {
     let body_str = String::from_utf8(body.to_vec()).unwrap();
     let events: Vec<GitHubEvent> = serde_json::from_str(&body_str).unwrap();
 
-    for event in events.iter() {
-        println!("Body: {:?}", event);
-    }
-
     return HttpResponse::Ok().json(events);
 }
 
@@ -59,8 +77,18 @@ async fn main() -> std::io::Result<()> {
 
     let port = env::var("PORT").expect("PORT is not defined.");
 
-    return HttpServer::new(|| App::new().service(get_activities_from_github))
-        .bind(format!("0.0.0.0:{}", port))?
-        .run()
-        .await;
+    return HttpServer::new(|| {
+        let cors = Cors::default()
+            .allowed_origin("http://localhost:8000")
+            .allowed_methods(vec!["GET"])
+            .max_age(3600);
+
+        App::new()
+            .wrap(cors)
+            .service(get_activities_from_twitter)
+            .service(get_activities_from_github)
+    })
+    .bind(format!("0.0.0.0:{}", port))?
+    .run()
+    .await;
 }
