e1d8cec40858a6f8a06d7c733fa3cda0e4aea6ca
Hoshi <hoshimure.47@gmail.com>
2021-01-14 23:14:03 +0900

    :recycle: module devide.

--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,13 @@
 mod github;
+mod twitter;
 
 use actix_cors::Cors;
-use actix_web::{
-    client::{Client, Connector},
-    get, App, HttpResponse, HttpServer, Responder,
-};
+use actix_web::{App, HttpServer};
 use dotenv::dotenv;
-use openssl::ssl::{SslConnector, SslMethod};
-use serde_json::Value;
 use std::env;
 
 use github::{get_activities_from_github};
-
-#[get("/v1/activities/twitter")]
-async fn get_activities_from_twitter() -> impl Responder {
-    let url = "https://api.twitter.com/2/users/1084336920/tweets?tweet.fields=created_at&expansions=author_id&user.fields=created_at&max_results=10";
-    let token = env::var("TWITTER_BEARER_TOKEN").expect("VAR is not defined");
-
-    let builder = SslConnector::builder(SslMethod::tls()).unwrap();
-    let client = Client::builder()
-        .header("User-Agent", "localhost")
-        .header("Authorization", format!("Bearer {}", token))
-        .connector(Connector::new().ssl(builder.build()).finish())
-        .finish();
-
-    let response = client.get(url).send().await;
-    let body = response.unwrap().body().await.unwrap();
-    let body_str = String::from_utf8(body.to_vec()).unwrap();
-    let events: Value = serde_json::from_str(&body_str).unwrap();
-
-    return HttpResponse::Ok().body(events);
-}
+use twitter::{get_activities_from_twitter};
 
 
 #[actix_web::main]
---
+++ src/twitter/service.rs
@@ -0,0 +1,25 @@
+use super::{Value};
+use actix_web::{client::{Client,Connector},get, Responder, HttpResponse};
+use openssl::ssl::{SslConnector, SslMethod};
+use std::env;
+
+#[get("/v1/activities/twitter")]
+pub async fn get_activities_from_twitter() -> impl Responder {
+    let url = "https://api.twitter.com/2/users/1084336920/tweets?tweet.fields=created_at,attachments,public_metrics,entities&expansions=author_id&user.fields=created_at&max_results=10";
+    let token = env::var("TWITTER_BEARER_TOKEN").expect("VAR is not defined");
+
+    let builder = SslConnector::builder(SslMethod::tls()).unwrap();
+    let client = Client::builder()
+        .header("User-Agent", "localhost")
+        .header("Authorization", format!("Bearer {}", token))
+        .connector(Connector::new().ssl(builder.build()).finish())
+        .finish();
+
+    let response = client.get(url).send().await;
+    let body = response.unwrap().body().await.unwrap();
+    let body_str = String::from_utf8(body.to_vec()).unwrap();
+    let events: Value = serde_json::from_str(&body_str).unwrap();
+
+    return HttpResponse::Ok().body(events);
+}
+
