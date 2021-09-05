686d6da56d79e452fe8679d83c6a54226922a364
Hoshi <hoshimure.47@gmail.com>
2021-09-05 23:14:47 +0900

    :+1: add some spotify models.

--- src/spotify/models.rs
+++ src/spotify/models.rs
@@ -1,6 +1,19 @@
 use serde::{Deserialize, Serialize};
 
 #[derive(Debug, Serialize, Deserialize)]
+pub struct Tracks {
+    href: String,
+    total: u64,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct Image {
+    width: u64,
+    height: u64,
+    url: String,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
 pub struct Owner {
     id: String,
     display_name: String,
@@ -14,6 +27,8 @@ pub struct Playlist {
     description: String,
     public: bool,
     owner: Owner,
+    images: Vec<Image>,
+    tracks: Tracks,
 }
 
 #[derive(Debug, Serialize, Deserialize)]
@@ -33,4 +48,17 @@ impl Response {
             playlists: spotify_response.items,
         }
     }
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct GetTokenRequest {
+    pub grant_type: String,
+    pub scope: String,
+}
+#[derive(Debug, Serialize, Deserialize)]
+pub struct GetTokenResponse {
+    pub access_token: String,
+    pub token_type: String,
+    pub expires_in: u64,
+    pub scope: Option<String>,
 }
